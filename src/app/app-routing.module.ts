2983bfedc81d71b00a369ac5f7ca4c5cddee60ea
HP <mariem.jemaiel@esprit.tn>
2025-03-05 02:31:45 +0100

    download

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddContractComponent} from "./front-office/add-contract/add-contract.component";
+import {ContractListComponent} from "./back-office/contract-list/contract-list.component";
+import {FrontOfficeContractsComponent} from "./front-office/front-office-contracts/front-office-contracts.component";
+import {UpdateContractComponent} from "./front-office/update-contract/update-contract.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'documents', component: AddContractComponent },
+  { path: 'contracts', component: ContractListComponent },
+  { path: 'update-contract/:id', component: UpdateContractComponent },
+  { path: 'front-office/contracts', component: FrontOfficeContractsComponent },
+  { path: 'front-office', loadChildren: () => import('./front-office/front-office.module').then(m => m.FrontOfficeModule) },
+  { path: 'back-office', loadChildren: () => import('./back-office/back-office.module').then(m => m.BackOfficeModule) },
+  { path: '', redirectTo: '/front-office', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/front-office' } // Fallback route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faHome, faUsers, faCog, faChartBar } from '@fortawesome/free-solid-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot([]),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    library.add(faBars, faHome, faUsers, faCog, faChartBar);
+  }
+}
